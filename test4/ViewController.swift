b6ee063e030263bf2b11545d90e57932ff583cdf
foxking <foxking@foxkingdeMacBook-Pro.local>
2014-07-16 11:07:29 +0800

    Initial Commit

---
+++ test4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test4
+//
+//  Created by foxking on 14-7-16.
+//  Copyright (c) 2014年 foxking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
